ce4db00e11f74c50c3e6a80b655a577a489b805d
Gerov, Michael <Michael.Gerov@firstdata.com>
2018-08-14 10:25:21 -0400

    working GET; DELETE skeleton created

--- src/main/java/org/mhgerov/readinglist/ReadingListRestController.java
+++ src/main/java/org/mhgerov/readinglist/ReadingListRestController.java
@@ -1,8 +1,13 @@
 package org.mhgerov.readinglist;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,4 +26,13 @@ public class ReadingListRestController {
 		readingListRepository.save(book);
 	}
 	
+	@GetMapping("/api")
+	public List<Book> getBookList(@RequestParam String reader) {
+		return readingListRepository.findByReader(reader);
+	}
+	
+	@DeleteMapping("/api")
+	public void deleteBook(@RequestParam Long id) {
+		
+	}
 }
