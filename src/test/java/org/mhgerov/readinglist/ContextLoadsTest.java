0444765342bdf7826429411718c8afc991c0bdff
Gerov, Michael <Michael.Gerov@firstdata.com>
2018-08-14 11:46:05 -0400

    controller tests created

--- src/test/java/org/mhgerov/readinglist/ContextLoadsTest.java
+++ src/test/java/org/mhgerov/readinglist/ContextLoadsTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ReadingListApplicationTests {
+public class ContextLoadsTest {
 
 	@Test
 	public void contextLoads() {
---
+++ src/test/java/org/mhgerov/readinglist/SmokeTest.java
@@ -0,0 +1,26 @@
+package org.mhgerov.readinglist;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmokeTest {
+	
+	@Autowired
+	private ReadingListController controller1;
+	
+	@Autowired
+	private ReadingListRestController controller2;
+	
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(controller1).isNotNull();
+		assertThat(controller2).isNotNull();
+	}
+}
