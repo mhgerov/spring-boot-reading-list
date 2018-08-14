08cd8910e334b8eefeae48cff4ce8874e1ca8d90
Gerov, Michael <Michael.Gerov@firstdata.com>
2018-08-14 08:07:51 -0400

    static routing fixed

--- src/main/java/org/mhgerov/readinglist/ReadingListRepository.java
+++ src/main/java/org/mhgerov/readinglist/ReadingListRepository.java
@@ -1,6 +1,8 @@
 package org.mhgerov.readinglist;
 
 import java.util.List;
+
+import org.mhgerov.readinglist.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReadingListRepository extends JpaRepository<Book, Long> {
