80448911b8e42822233d3dd934b6f2a59f7a5860
Gerov, Michael <Michael.Gerov@firstdata.com>
2018-08-20 07:48:52 -0400

    custom jdbc url

--- src/main/java/org/mhgerov/readinglist/ReadingListApplication.java
+++ src/main/java/org/mhgerov/readinglist/ReadingListApplication.java
@@ -1,12 +1,17 @@
 package org.mhgerov.readinglist;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReadingListApplication {
+	
+	static Logger logger = LoggerFactory.getLogger(ReadingListApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReadingListApplication.class, args);
+		logger.info("My first custom INFO log!");
 	}
 }
