86a7e05a313bfd33a1d19c02f693b03b9debb375
Gerov, Michael <Michael.Gerov@firstdata.com>
2018-08-27 11:10:52 -0400

    init db service test class

--- src/test/java/org/mhgerov/readinglist/ApiControllerTest.java
+++ src/test/java/org/mhgerov/readinglist/ApiControllerTest.java
@@ -18,13 +18,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ReadingListApiController.class)
-@ActiveProfiles("test")
 public class ApiControllerTest {
 
 	@Autowired
