148538232679b170052b2c1c47d2231ca6811963
Gerov, Michael <Michael.Gerov@firstdata.com>
2018-08-27 08:42:53 -0400

    first reponse status specified

--- src/main/java/org/mhgerov/readinglist/ReadingListApiController.java
+++ src/main/java/org/mhgerov/readinglist/ReadingListApiController.java
@@ -3,38 +3,38 @@ package org.mhgerov.readinglist;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReadingListApiController {
-	
+
 	@Autowired
 	private ReadingListGateway readingListGateway;
-	
-	//private final BookDatabaseService bookDatabaseService;
-	
 
+	// private final BookDatabaseService bookDatabaseService;
 
 	@PostMapping("/api")
-	public void addBook(@RequestBody Book book) {
-		readingListGateway.addBook(book);
+	@ResponseStatus(HttpStatus.CREATED)
+	public void addBook(@RequestBody final Book book) {
+		this.readingListGateway.addBook(book);
 	}
-	
-	
+
 	@GetMapping("/api")
-	public List<Book> getBookList(@RequestParam String reader) {
-		//return bookDatabaseService.getListByReader(reader);
-		return readingListGateway.getReadingList(reader);
-	
+	public List<Book> getBookList(@RequestParam final String reader) {
+		// return bookDatabaseService.getListByReader(reader);
+		return this.readingListGateway.getReadingList(reader);
+
 	}
-	
+
 	@DeleteMapping("/api")
-	public void deleteBook(@RequestParam long id) {
-		readingListGateway.deleteBook(id);
+	public void deleteBook(@RequestParam final long id) {
+		this.readingListGateway.deleteBook(id);
 	}
 }
