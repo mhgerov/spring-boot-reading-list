cc3c9b426ddbbad456ec720bafab03baa2ea4d09
Gerov, Michael <Michael.Gerov@firstdata.com>
2018-08-27 10:36:43 -0400

    add config class

--- src/main/java/org/mhgerov/readinglist/BookApplicationConfiguration.java
+++ src/main/java/org/mhgerov/readinglist/BookApplicationConfiguration.java
@@ -2,8 +2,10 @@ package org.mhgerov.readinglist;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!test")
 @ImportResource("classpath:integration.xml")
 public class BookApplicationConfiguration {
 
